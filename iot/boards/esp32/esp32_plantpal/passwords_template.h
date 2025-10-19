39df24db4e8caeab13e7d001d382ff8f5785eeaa
reecewayt <reecewayt96@gmail.com>
2025-10-19 11:37:21 -0700

    feat[iot development] Add config objects, schemas, and poc arduino application that can both send and receive message to google pub/sub

---
+++ iot/boards/esp32/esp32_plantpal/passwords_template.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// ============================================
+// WiFi Configuration
+// Contains sensitive information, copy to passwords.h
+// and do not commit to version control. Uncomment with credentials.
+// ============================================
+//#define WIFI_SSID       "YOUR_WIFI_SSID"
+//#define WIFI_PASSWORD   "YOUR_WIFI_PASSWORD"
