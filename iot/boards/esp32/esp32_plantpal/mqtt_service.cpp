0fe24ee7f81d79d78b038d9ad3a6f01d80942299
reecewayt <reecewayt96@gmail.com>
2025-10-20 11:24:58 -0700

    [feat(iot development)] Remove dummy demo application logic, replaced with app logic and valid subscriptions  we will use in plantpal; topics have been created in google pub/sub and hivemq config template is updated with those changes as well

--- iot/boards/esp32/esp32_plantpal/mqtt_service.cpp
+++ iot/boards/esp32/esp32_plantpal/mqtt_service.cpp
@@ -1,24 +1,27 @@
-#include "config.h"
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include <MQTT.h>
+#include "mqtt_service.h"
 
+const String TAG = "MQTT_SERVICE";
 
 void messageReceived(String &topic, String &payload) {
 
     // `topic` is an Arduino String, which can't be used in a switch statement.
     // Use if/else string comparisons instead.
     if (topic == SUB_MOISTURE_TOPIC) {
-        // TODO: Handle soil moisture request
+        DEBUG_LOG(TAG, "Received moisture sensor reading request");
+        DEBUG_LOG(TAG, payload);
+        // TODO: Handle moisture sensor request
     }
     else if (topic == SUB_WATER_TOPIC) {
+        DEBUG_LOG(TAG, "Received water plant request");
+        DEBUG_LOG(TAG, payload);
         // TODO: Handle water level request
     }
     else {
         // Unknown topic
         // TODO: Handle unknown topic
+        DEBUG_LOG(TAG, String("Received message on unknown topic: ") + topic);
+        DEBUG_LOG(TAG, payload);
     }
-
 }
 
 /**
